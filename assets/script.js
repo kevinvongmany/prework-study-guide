98ba5c0d93321419bd85b5a3795e2664b71fb5a5
Kevin Vongmany <kevin.vongmany@techmedia.com.au>
2024-03-29 14:39:37 +1100

    Updated JS code into webpage

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript']
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log("Topic not found, let's try again!");
+}
+*/
+for (var idx = 0; idx < topics.length; idx++) {
+    console.log(topics[idx])
+}
